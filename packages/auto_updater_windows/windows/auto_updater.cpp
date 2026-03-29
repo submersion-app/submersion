ac080f53ead363e94db87a68fbb2287de87abc3e
Eric Griffin <ericgriffin@gmail.com>
2026-03-28 20:52:47 -0400

    fix: thread-safe event queue in auto_updater.cpp (#83)
    WinSparkle fires callbacks on background threads. Replace direct
    event_sink_->Success() calls with a mutex-guarded queue + PostMessage
    to defer delivery to the Flutter platform thread.

--- packages/auto_updater_windows/windows/auto_updater.cpp
+++ packages/auto_updater_windows/windows/auto_updater.cpp
@@ -1,14 +1,24 @@
 #include "WinSparkle-0.8.1/include/winsparkle.h"
 
+#include <windows.h>
+
 #include <flutter/event_channel.h>
 #include <flutter/method_channel.h>
 #include <flutter/plugin_registrar_windows.h>
 #include <flutter/standard_method_codec.h>
 
 #include <memory>
+#include <mutex>
+#include <queue>
 #include <sstream>
+#include <string>
 
 namespace {
+
+// Custom Windows message used to signal the platform thread that events are
+// waiting in the queue. WM_APP is the start of the application-private range.
+constexpr UINT WM_APP_SPARKLE_EVENT = WM_APP + 1;
+
 // Forward declarations for WinSparkle callbacks
 void __onErrorCallback();
 void __onShutdownRequestCallback();
@@ -28,18 +38,34 @@ class AutoUpdater {
 
   virtual ~AutoUpdater();
 
-  void AutoUpdater::SetFeedURL(std::string feedURL);
-  void AutoUpdater::CheckForUpdates();
-  void AutoUpdater::CheckForUpdatesWithoutUI();
-  void AutoUpdater::SetScheduledCheckInterval(int interval);
+  void SetFeedURL(std::string feedURL);
+  void CheckForUpdates();
+  void CheckForUpdatesWithoutUI();
+  void SetScheduledCheckInterval(int interval);
 
-  void AutoUpdater::RegisterEventSink(
+  void RegisterEventSink(
       std::unique_ptr<flutter::EventSink<flutter::EncodableValue>> ptr);
-  void AutoUpdater::OnWinSparkleEvent(std::string eventName);
+
+  // Called from WinSparkle background threads. Enqueues the event and signals
+  // the platform thread via PostMessage.
+  void OnWinSparkleEvent(std::string eventName);
+
+  // Called from the platform thread (window proc delegate) to drain the queue
+  // and deliver events through the EventSink.
+  void DrainEvents();
+
+  // Store the HWND used for PostMessage signaling.
+  void SetWindowHandle(HWND hwnd);
 
  private:
   static AutoUpdater* lazySingleton;
   std::unique_ptr<flutter::EventSink<flutter::EncodableValue>> event_sink_;
+
+  // Thread-safe event queue. WinSparkle callbacks push event names here;
+  // the platform thread drains them in DrainEvents().
+  std::mutex event_mutex_;
+  std::queue<std::string> event_queue_;
+  HWND hwnd_ = nullptr;
 };
 
 AutoUpdater* AutoUpdater::lazySingleton = nullptr;
@@ -58,6 +84,10 @@ AutoUpdater::AutoUpdater() {
 
 AutoUpdater::~AutoUpdater() {}
 
+void AutoUpdater::SetWindowHandle(HWND hwnd) {
+  hwnd_ = hwnd;
+}
+
 void AutoUpdater::SetFeedURL(std::string feedURL) {
   win_sparkle_set_appcast_url(feedURL.c_str());
   win_sparkle_init();
@@ -67,12 +97,6 @@ void AutoUpdater::SetFeedURL(std::string feedURL) {
   win_sparkle_set_did_find_update_callback(__onDidFindUpdateCallback);
   win_sparkle_set_did_not_find_update_callback(__onDidNotFindUpdateCallback);
   win_sparkle_set_update_cancelled_callback(__onUpdateCancelledCallback);
-
-  // TODO: These will be supported once we update WinSparkle to >0.8.0
-  // win_sparkle_set_update_skipped_callback(__onUpdateSkippedCallback);
-  // win_sparkle_set_update_postponed_callback(__onUpdatePostponedCallback);
-  // win_sparkle_set_update_dismissed_callback(__onUpdateDismissedCallback);
-  // win_sparkle_set_user_run_installer_callback(__onUserRunInstallerCallback);
 }
 
 void AutoUpdater::CheckForUpdates() {
@@ -95,11 +119,35 @@ void AutoUpdater::RegisterEventSink(
 }
 
 void AutoUpdater::OnWinSparkleEvent(std::string eventName) {
-  if (event_sink_ == nullptr)
-    return;
-  flutter::EncodableMap args = flutter::EncodableMap();
-  args[flutter::EncodableValue("type")] = eventName;
-  if (event_sink_) {
+  {
+    std::lock_guard<std::mutex> lock(event_mutex_);
+    event_queue_.push(std::move(eventName));
+  }
+
+  // Signal the platform thread. PostMessage is safe to call from any thread.
+  if (hwnd_ != nullptr) {
+    PostMessage(hwnd_, WM_APP_SPARKLE_EVENT, 0, 0);
+  }
+}
+
+void AutoUpdater::DrainEvents() {
+  // Swap the queue under the lock to minimize time spent holding the mutex.
+  std::queue<std::string> local_queue;
+  {
+    std::lock_guard<std::mutex> lock(event_mutex_);
+    std::swap(local_queue, event_queue_);
+  }
+
+  // Now deliver events outside the lock, on the platform thread.
+  while (!local_queue.empty()) {
+    std::string eventName = std::move(local_queue.front());
+    local_queue.pop();
+
+    if (event_sink_ == nullptr)
+      continue;
+
+    flutter::EncodableMap args = flutter::EncodableMap();
+    args[flutter::EncodableValue("type")] = eventName;
     event_sink_->Success(flutter::EncodableValue(args));
   }
 }
