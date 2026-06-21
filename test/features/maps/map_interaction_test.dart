3a526abfb58afdc73077fbf0dc79dd0d82db6007
Eric Griffin <ericgriffin@gmail.com>
2026-06-21 02:49:05 -0400

    chore(maps): remove unused import in map_interaction_test (#238)

--- test/features/maps/map_interaction_test.dart
+++ test/features/maps/map_interaction_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:latlong2/latlong.dart';
